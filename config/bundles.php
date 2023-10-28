f69d35b0072a635285f7005866a751f6d4532d71
Kamil Kurek <kamil.kurek1212@gmail.com>
2023-10-28 13:47:51 +0200

    downgrade on mysql version, removed mercury

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,5 @@ return [
     Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
